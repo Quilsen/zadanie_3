5139002161b91803c4362b4d962b9b4da38de86e
tomaszmlodzianowski <t.mlodzianowski@gmail.com>
2022-04-02 22:24:10 +0200

    testowanie

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,6 +10,12 @@ public class Main {
         UniquePicture uP = new UniquePicture();
         System.out.println(uP.add(new Point(1,1,"Punkt")));
         System.out.println(uP.add(new Point(1,1,"Punkt")));
+        System.out.println(uP.add(new Circle(new Point(1,1,"srodek"),10,"kółko")));
+        System.out.println(uP.add(new Circle(new Point(1,1,"srodek"),10,"kółko")));
+        System.out.println("\n");
+        StandarizedPicture sP = new StandarizedPicture();
+        System.out.println(sP.add(new Point(1,1,"PunkTcik123")));
+        System.out.println(sP.add(new Point(1,1,"P2UN3K4T5CIK123")));
 
     }
 }
